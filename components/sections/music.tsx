df21296dfa2e0ffdc1d5530070fab27ffa9c9484
Isaac <kch062522@gmail.com>
2025-02-22 21:24:14 +0800

    fix: fix music section title size

--- components/sections/music.tsx
+++ components/sections/music.tsx
@@ -4,7 +4,7 @@ export function Music() {
   return (
     <Section id="music" backgroundImage="/images/music.jpeg">
       <div className="mx-auto flex h-full max-w-lg flex-col items-start justify-center">
-        <h1 className="mb-8 text-6xl font-bold">Recent Loops</h1>
+        <h1 className="mb-8 text-4xl font-bold">Recent Loops</h1>
         <iframe
           className="rounded-lg"
           src="https://open.spotify.com/embed/track/27udJcfu06TvbbOpgfxIlw?utm_source=generator&theme=0"
