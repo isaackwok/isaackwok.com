1aabc76356daa4e1f2e1c1de42a10ba6ae5f7b3f
Isaac <kch062522@gmail.com>
2025-02-23 14:01:35 +0800

    feat: add speed insight plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavigationPanel } from "@/components/navigation";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         {children}
         <NavigationPanel />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
